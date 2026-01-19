697cb375a7811eeb453592169198872ab7fe01e0
thiagoeduardoc <thiagoeduardoc@gmail.com>
2026-01-19 09:04:43 -0300

    Modelagem do banco de dados, ninjaModel e MissoesModel concluidos, anotations feitas para relacionamentos de tabela incluidos na main

--- src/main/java/dev/Java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/Java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -3,6 +3,8 @@ package dev.Java10x.CadastroDeNinjas.Missoes;
 import dev.Java10x.CadastroDeNinjas.Ninja.NinjaModel;
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 @Table(name = "tb_missoes")
 public class MissoesModel {
