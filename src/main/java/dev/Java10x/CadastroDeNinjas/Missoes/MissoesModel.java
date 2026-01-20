48590f4d6d103e9a4b7eda608789aaca7529e4bc
thiagoeduardoc <thiagoeduardoc@gmail.com>
2026-01-20 10:29:15 -0300

    feat: Coloca lombok na classe para abstracao

--- src/main/java/dev/Java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/Java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,11 +2,17 @@ package dev.Java10x.CadastroDeNinjas.Missoes;
 
 import dev.Java10x.CadastroDeNinjas.Ninja.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesModel {
 
     @Id
--- src/main/java/dev/Java10x/CadastroDeNinjas/Ninja/NinjaModel.java
+++ src/main/java/dev/Java10x/CadastroDeNinjas/Ninja/NinjaModel.java
@@ -2,10 +2,16 @@ package dev.Java10x.CadastroDeNinjas.Ninja;
 
 import dev.Java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 // entity transforma uma classe em entidade do BD
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
 
     @Id
@@ -19,38 +25,5 @@ public class NinjaModel {
     @ManyToOne
     @JoinColumn(name = "missoes_id") // foreign key
     private MissoesModel missoes;
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
 
