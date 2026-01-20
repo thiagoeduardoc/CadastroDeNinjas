68cce52a2a24524bcbd21b4cac265ebdafe5cba4
thiagoeduardoc <thiagoeduardoc@gmail.com>
2026-01-20 10:34:25 -0300

    fix: Coloca a coluna de email como UNIQUE

--- src/main/java/dev/Java10x/CadastroDeNinjas/Ninja/NinjaModel.java
+++ src/main/java/dev/Java10x/CadastroDeNinjas/Ninja/NinjaModel.java
@@ -18,6 +18,7 @@ public class NinjaModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+    @Column(unique = true)
     private String email;
     private int idade;
 
