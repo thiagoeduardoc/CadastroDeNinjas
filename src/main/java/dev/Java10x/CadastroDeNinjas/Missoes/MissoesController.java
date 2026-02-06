bc9d4d420d95d0e67844f6e00ee351511b93408d
thiagoeduardoc <thiagoeduardoc@gmail.com>
2026-02-06 20:11:52 -0300

    feat: Adiciona configurações usando MissoesDTO ao inves de MissoesModel, além do uso de ResponseEntity no MissoesController

--- src/main/java/dev/Java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/Java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,5 +1,8 @@
 package dev.Java10x.CadastroDeNinjas.Missoes;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,25 +18,50 @@ public class MissoesController {
 
     // Get -- mandar uma requisição para mostrar missoes
     @GetMapping("/listar")
-    public List<MissoesModel> listarMissoes(){
-        return missoesService.listarMissoes();
+    public ResponseEntity<List<MissoesDTO>>  listarMissoes(){
+        List<MissoesDTO> missoes = missoesService.listarMissoes();
+        return ResponseEntity.ok(missoes);
+    }
+
+    @GetMapping("/listar/{id}")
+    public ResponseEntity<?> listarMissaoId(@PathVariable Long id){
+        if (missoesService.listarMissaoId(id) != null) {
+            MissoesDTO missoesDTO = missoesService.listarMissaoId(id);
+            return ResponseEntity.ok(missoesDTO);
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body("Missão não encontrada");
+        }
     }
 
     // post -- mandar uma requisição para criar missoes
     @PostMapping("/criar")
-    public MissoesModel criarMissao(@RequestBody MissoesModel missao){
+    public MissoesDTO criarMissao(@RequestBody MissoesDTO missao){
         return missoesService.criarMissao(missao);
     }
 
     // Put -- mandar uma requisição para alterar missoes
-    @PutMapping("/alterar")
-    public String alterarMissao(){
-        return  "Missao alterada";
+    @PutMapping("/alterar/{id}")
+    public ResponseEntity<?> alterarMissao(@PathVariable Long id, @RequestBody MissoesDTO missao){
+        if (missoesService.listarMissaoId(id) != null) {
+            MissoesDTO missoesDTO = missoesService.atualizarMissao(id, missao);
+            return ResponseEntity.ok(missoesDTO);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Missão não encontrada");
+        }
     }
 
     // Delete -- mandar uma requisição para deletar missoes
     @DeleteMapping("/deletar/{id}")
-    public void deletarMissao(@PathVariable Long id){
-        missoesService.deletarMissao(id);
+    public ResponseEntity<String> deletarMissao(@PathVariable Long id){
+        if (missoesService.listarMissaoId(id) != null){
+            missoesService.deletarMissao(id);
+            return ResponseEntity.ok("Missão Deletada");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Missao não encontrada");
+        }
     }
 }
--- src/main/java/dev/Java10x/CadastroDeNinjas/Missoes/MissoesService.java
+++ src/main/java/dev/Java10x/CadastroDeNinjas/Missoes/MissoesService.java
@@ -1,26 +1,54 @@
 package dev.Java10x.CadastroDeNinjas.Missoes;
 
+import dev.Java10x.CadastroDeNinjas.Ninja.NinjaModel;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class MissoesService {
     private final MissoesRepository missoesRepository;
+    private MissoesMapper missoesMapper;
 
-    public MissoesService(MissoesRepository missoesRepository) {
+    public MissoesService(MissoesRepository missoesRepository, MissoesMapper missoesMapper) {
         this.missoesRepository = missoesRepository;
+        this.missoesMapper = missoesMapper;
     }
 
     // Listar
-    public List<MissoesModel> listarMissoes(){
-        return missoesRepository.findAll();
+    public List<MissoesDTO> listarMissoes(){
+        List<MissoesModel> missoesModel = missoesRepository.findAll();
+        return missoesModel.stream()
+                .map(missoesMapper::map)
+                .collect(Collectors.toList());
+    }
+
+    public MissoesDTO listarMissaoId(Long id){
+        Optional<MissoesModel> missaoId = missoesRepository.findById(id);
+        return missaoId.map(missoesMapper::map).orElse(null);
     }
 
     // Criar
-    public MissoesModel criarMissao(MissoesModel missao){
-        return missoesRepository.save(missao);
+    public MissoesDTO criarMissao(MissoesDTO missaoDTO){
+        MissoesModel missao = missoesMapper.map(missaoDTO);
+        missao = missoesRepository.save(missao);
+        return missoesMapper.map(missao);
+    }
+
+    // Atualizar
+    public MissoesDTO atualizarMissao(Long id, MissoesDTO missoesDTO){
+        Optional<MissoesModel> missaoExiste = missoesRepository.findById(id);
+        if (missaoExiste.isPresent()){
+            MissoesModel missao = missoesMapper.map(missoesDTO);
+            missao.setId(id);
+            MissoesModel missaoSalva = missoesRepository.save(missao);
+            return missoesMapper.map(missaoSalva);
+        } else {
+            return null;
+        }
     }
 
     // Deletar
