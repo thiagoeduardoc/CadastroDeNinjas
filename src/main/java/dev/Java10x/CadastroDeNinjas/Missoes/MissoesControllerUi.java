31bbe24770eb8c4dfecbcd53a82e3820c5672489
thiagoeduardoc <thiagoeduardoc@gmail.com>
2026-02-14 18:59:59 -0300

    feat: Adiciona UI para adicionar e ver detalhes das missoes

--- src/main/java/dev/Java10x/CadastroDeNinjas/Missoes/MissoesControllerUi.java
+++ src/main/java/dev/Java10x/CadastroDeNinjas/Missoes/MissoesControllerUi.java
@@ -1,11 +1,12 @@
 package dev.Java10x.CadastroDeNinjas.Missoes;
 
+import dev.Java10x.CadastroDeNinjas.Ninja.NinjaDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -28,6 +29,31 @@ public class MissoesControllerUi {
     @GetMapping("/deletar/{id}")
     public String deletarMissao(@PathVariable Long id){
         missoesService.deletarMissao(id);
+        return "redirect:/missoes/ui/listar";
+    }
+
+    @GetMapping("/listar/{id}")
+    public String listarMissaoId(@PathVariable Long id, Model model){
+        MissoesDTO missao = missoesService.listarMissaoId(id);
+        if (missoesService.listarMissaoId(id) != null){
+            model.addAttribute("missao", missao);
+            return "detalhesmissao";
+        } else {
+            model.addAttribute("mensagem", "Missão não encontrada");
+            return "listarmissoes";
+        }
+    }
+
+    @GetMapping("/adicionar")
+    public String formularioAdicionarMissao(Model model){
+        model.addAttribute("missao", new MissoesDTO());
+        return "adicionarmissao";
+    }
+
+    @PostMapping("/salvar")
+    public String salvarMissao(@ModelAttribute MissoesDTO missao, RedirectAttributes redirectAttributes){
+        missoesService.criarMissao(missao);
+        redirectAttributes.addFlashAttribute("mensagem", "Missao cadastrada");
         return "redirect:/missoes/ui/listar";
     }
 }
